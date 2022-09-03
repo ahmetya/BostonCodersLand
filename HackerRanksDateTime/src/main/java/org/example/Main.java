f9f73e9fba802ac7d8e5b90d1f43f79f2b6f7e2b
Ahmet Yalcinkaya <ahmetyky@gmail.com>
2022-09-03 20:42:34 +0200

    Hackerank solution update

--- HackerRanksDateTime/src/main/java/org/example/Main.java
+++ HackerRanksDateTime/src/main/java/org/example/Main.java
@@ -7,8 +7,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Day");
         int day = scan.nextInt();
+        System.out.println("Enter Month");
         int month = scan.nextInt();
+        System.out.println("Enter Year");
         int year = scan.nextInt();
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.DATE, day);
